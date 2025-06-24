e3be35d2c4856ebdbcf00bf1c68a9342b325c695
Łukasz Sławęta <lukasz.slaweta@gmail.com>
2025-06-24 12:48:12 +0200

    Initial commit – upload local project

---
+++ inventoryservice/src/main/java/com/example/projekt/inventoryservice/repository/VenueRepository.java
@@ -0,0 +1,9 @@
+package com.example.projekt.inventoryservice.repository;
+
+import com.example.projekt.inventoryservice.entity.Venue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VenueRepository extends JpaRepository<Venue, Long> {
+}
---
+++ inventoryservice/src/main/java/com/example/projekt/inventoryservice/service/InventoryService.java
@@ -0,0 +1,46 @@
+package com.example.projekt.inventoryservice.service;
+
+
+import com.example.projekt.inventoryservice.entity.Event;
+import com.example.projekt.inventoryservice.entity.Venue;
+import com.example.projekt.inventoryservice.repository.EventRepository;
+import com.example.projekt.inventoryservice.repository.VenueRepository;
+import com.example.projekt.inventoryservice.response.EventInventoryResponse;
+import com.example.projekt.inventoryservice.response.VenueInventoryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class InventoryService {
+
+    private final EventRepository eventRepository;
+    private final VenueRepository venueRepository;
+
+    @Autowired
+    public InventoryService(final EventRepository eventRepository, final VenueRepository venueRepository) {
+        this.eventRepository = eventRepository;
+        this.venueRepository = venueRepository;
+    }
+
+    public List<EventInventoryResponse> getAllEvents() {
+        final List<Event> events = eventRepository.findAll();
+
+        return events.stream().map(event -> EventInventoryResponse.builder()
+                .event(event.getName())
+                .capacity(event.getLeftCapacity())
+                .venue(event.getVenue())
+                .build()).collect(Collectors.toList());
+    }
+    public VenueInventoryResponse getVenueInformation(final Long venueId) {
+        final Venue venue = venueRepository.findById(venueId).orElse(null);
+
+        return VenueInventoryResponse.builder()
+                .venueId(venue.getId())
+                .venueName(venue.getName())
+                .totalCapacity(venue.getTotalCapacity())
+                .build();
+    }
+}
